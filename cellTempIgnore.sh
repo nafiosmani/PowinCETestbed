3e53ed5c0c25f6b473727e80095d131afef1f060
flydthbrbr <33043010+flydthbrbr@users.noreply.github.com>
2024-12-03 15:44:09 -0600

    Update cellTempIgnore.sh
    added header and cleaned up the script.

--- cellTempIgnore.sh
+++ cellTempIgnore.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # ~Forward~
 #
 # 1) Purpose
@@ -6,11 +7,20 @@
 # 2) Parameters (in order)
 #
 # Standard Use
-# $1 
+#       ./cellTempIgnore.sh <array> <string> <bp> <cell>
 #
 # 3) Resulting actions
+#   Ignore the cell temp for the identified cell.
 #
 # 4) Outputs
-#
+#  OK response
 
-for $1 in {1..2}; do for $2 in {1..18}; do for $3 in {1..14}; do for $4 in {1..30}; do curl http://10.0.1.1:8080/turtle/tools/controls/bms/array/$1/string/$2/bp/$3/cg/$4/ignorecellgrouptemp/on; done; done; done; done
+for $1 in {1..2}; 
+    do for $2 in {1..18}; 
+        do for $3 in {1..14}; 
+            do for $4 in {1..30}; 
+                do curl http://10.0.1.1:8080/turtle/tools/controls/bms/array/$1/string/$2/bp/$3/cg/$4/ignorecellgrouptemp/on; 
+           done; 
+        done; 
+    done; 
+done
