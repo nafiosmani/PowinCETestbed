24c9d51afc247a468102ef42ef6cb51c8cfd1447
flydthbrbr <33043010+flydthbrbr@users.noreply.github.com>
2024-12-04 17:31:21 -0600

    Update master.sh
    Added more options

--- master.sh
+++ master.sh
@@ -11,22 +11,29 @@ while [[ "$REPLY" != 0 ]]; do
     cat << EOM
     Please select one:
 
-        0. Quit
     	1. Display Lineup Feather Status
-	2. Deploy Late baseline
-    	3. Datanab
-        4. 
+	    2. Deploy Late baseline
+    	3. Deploy Late Hatchery
+        4. Set Fan Speed
+        5. Time Server Check
+        6. UPS Config
+        7. Cell Temp Ignore
+        8. ATHD
+        9. Swap Feather RS485 Driver
+        0. Quit
+
 EOM
 
 read -p "Enter Selection > "
 if [[ "$REPLY" =~ [0-3]$ ]]; then
 
     if [[ "$REPLY" == 0 ]]; then
-        sleep "$DELAY"
+        sleep 3
         break
     fi
     if [[ "$REPLY" == 1 ]]; then
-        ./new_feather_test.sh
+        read -p "Which LN? > " __LN
+        echo __LN | ./new_feather_test.sh
         sleep "$DELAY"
         continue
     fi
@@ -37,7 +44,37 @@ if [[ "$REPLY" =~ [0-3]$ ]]; then
         continue
     fi
     if [[ "$REPLY" == 3 ]]; then
-        echo "do some other stuff"
+        echo "Deploy Late hatchery"
+        sleep "$DELAY"
+        continue
+    fi
+    if [[ "$REPLY" == 4 ]]; then
+        echo "Set Fan Speed"
+        sleep "$DELAY"
+        break
+    fi
+    if [[ "$REPLY" == 5 ]]; then
+        echo "Time Server Check"
+        sleep "$DELAY"
+        continue
+    fi
+    if [[ "$REPLY" == 6 ]]; then
+        echo "UPS Config"
+        sleep "$DELAY"
+        continue
+    fi
+    if [[ "$REPLY" == 7 ]]; then
+        echo "Cell Temp Ignore"
+        sleep "$DELAY"
+        continue
+    fi    
+    if [[ "$REPLY" == 8 ]]; then
+        echo "ATHD"
+        sleep "$DELAY"
+        break
+    fi
+    if [[ "$REPLY" == 9 ]]; then
+        echo "Swap Feather RS485 Drive"
         sleep "$DELAY"
         continue
     fi
